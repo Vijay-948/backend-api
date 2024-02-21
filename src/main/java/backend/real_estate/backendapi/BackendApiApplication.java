dd974c46af77b722fdde537e89578a22366258e6
vijay <vijay@gmail.com>
2024-02-21 12:42:20 +0530

    frist-commit

---
+++ src/main/java/backend/real_estate/backendapi/BackendApiApplication.java
@@ -0,0 +1,12 @@
+package backend.real_estate.backendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiApplication.class, args);
+	}
+}
---
+++ src/test/java/backend/real_estate/backendapi/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.real_estate.backendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
