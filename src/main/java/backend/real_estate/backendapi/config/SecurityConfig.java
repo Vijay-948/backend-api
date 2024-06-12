fe8ec430e41c0a37e365710f39b8f94c09776928
Vijay-948 <kvijayreddy948@gmail.com>
2024-06-12 15:22:54 +0530

    integration

--- src/main/java/backend/real_estate/backendapi/config/SecurityConfig.java
+++ src/main/java/backend/real_estate/backendapi/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://customer-product.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
